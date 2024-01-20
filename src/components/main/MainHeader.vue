f33c99c0ee78d3f5f99141bc3d6336f1811119a3
Tim <themstede@st.noorderpoort.nl>
2024-01-20 22:32:53 +0100

    Firebase storage global var initialized

--- src/components/main/MainHeader.vue
+++ src/components/main/MainHeader.vue
@@ -1,12 +1,29 @@
 <template>
-    <header class="w-20 h-20 bg-black">
-
+    <header class="bg-[#F1EFE7]">
+        <img :src="imgUrl" alt="MINIMOIS LOGO" class="h-20">
     </header>
 </template>
 
 <script>
+import { ref, getDownloadURL } from 'firebase/storage';
 
 export default {
     name: "Jammie",
+    data() {
+        return {
+            imgUrl: null
+        }
+    },
+    mounted() {
+        const storageRef = ref(this.$storage, 'minimoislogo.png')
+        
+        getDownloadURL(storageRef)
+            .then((url) => {
+                this.imgUrl = url
+            })
+            .catch(error => {
+                console.error('Error getting download URL: ', error);
+            })
+    }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,30 @@
 import './assets/main.css'
 
+import { getStorage } from 'firebase/storage'
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyBVESzUe1dEQMco3W603xU5tfkVlSDKCKk',
+    authDomain: 'vuemo-grasp.firebaseapp.com',
+    projectId: 'vuemo-grasp',
+    storageBucket: 'vuemo-grasp.appspot.com',
+    messagingSenderId: '320551227573',
+    appId: '1:320551227573:web:7a424f12eabf090951aefc',
+    measurementId: 'G-NL7MFJW3QH'
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const storage = getStorage(firebaseApp);
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App);
+app.config.globalProperties.$storage = storage;
+
+app.use(router);
+app.mount('#app');
 
 
 
